e949f74eb8e1e0aa9508c663b899efe5bc9ff7fe
dima_z <dimazvazhii@gmail.com>
2020-03-24 13:58:13 +0200

    integrated security sample

--- Flexmonster Accelerator MVC/App_Start/FlexmonsterConfig.cs
+++ Flexmonster Accelerator MVC/App_Start/FlexmonsterConfig.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Flexmonster.Accelerator.Utils;
+using Flexmonster.Accelerator.Utils.Auth;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,10 +13,25 @@ namespace Flexmonster_Accelerator_MVC.App_Start
         {
             // Replace with actual data source. 
             // Example: Data Source=localhost
-            Flexmonster.Accelerator.Controllers.FlexmonsterProxyController.ConnectionString = "Data Source=https://olap.flexmonster.com/olap/msmdpump.dll";
+            Flexmonster.Accelerator.Controllers.FlexmonsterProxyController.ConnectionString = "Data Source=http://localhost/OLAP-M/msmdpump.dll";
             Flexmonster.Accelerator.Utils.CacheManager.MemoryLimit = 10 * 1024 * 1024; // Mb to bytes
             Flexmonster.Accelerator.Utils.CacheManager.Enabled = true;
             //Flexmonster.Accelerator.Utils.LoggerLocator.SetLogger(new Flexmonster.Accelerator.Utils.ILogger());
+        }
+    }
+
+    public class WindowsAuthHelper : IAuthHelper
+    {
+        public string effectiveUserName;
+        public WindowsAuthHelper(string effectiveUserName)
+        {
+            this.effectiveUserName = effectiveUserName;
+        }
+
+        public void AddAuth(ConnectionStringBuilder connectionBuilder)
+        {
+            connectionBuilder.Set("EffectiveUserName", this.effectiveUserName);
+            connectionBuilder.Set("Impersonation Level", "Impersonate");
         }
     }
 }
--- Flexmonster Accelerator MVC/Controllers/AcceleratorController.cs
+++ Flexmonster Accelerator MVC/Controllers/AcceleratorController.cs
@@ -1,17 +1,32 @@
-﻿using System;
+﻿using Flexmonster_Accelerator_MVC.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Controllers;
 
 namespace Flexmonster_Accelerator_MVC.Controllers
 {
+    [AuthAttribute]
     public class AcceleratorController : Flexmonster.Accelerator.Controllers.FlexmonsterProxyController
     {
+
         public IHttpActionResult Test()
         {
             return Ok("Test");
         }
+    }
+
+    class AuthAttribute : AuthorizeAttribute
+    {
+
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            AcceleratorController.AuthHelper = new WindowsAuthHelper(actionContext.RequestContext.Principal.Identity.Name.ToString().Replace("//", "\\"));
+            return true;
+        }
+
     }
 }
