f0c95dc1ebe857528f6d5a5b96b3c1a758f27e83
olena <olena.krysan@gmail.com>
2020-09-14 19:01:13 +0300

    Update AcceleratorController.cs

--- Flexmonster Accelerator MVC/Controllers/AcceleratorController.cs
+++ Flexmonster Accelerator MVC/Controllers/AcceleratorController.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using Flexmonster.Accelerator.Models;
 
 namespace Flexmonster_Accelerator_MVC.Controllers
 {
     public class AcceleratorController : Flexmonster.Accelerator.Controllers.FlexmonsterProxyController
     {
-        public IHttpActionResult Test()
+        public override void OnRequest(BaseArgs args)
         {
-            return Ok("Test");
+            base.OnRequest(args);
         }
     }
 }
