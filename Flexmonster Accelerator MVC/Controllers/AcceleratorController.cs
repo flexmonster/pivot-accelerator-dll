2a57d2de85314a70755ee13c68a87b4d0de308e3
Ian Sadovy <ian.sadovy@gmail.com>
2020-11-30 14:29:39 +0200

    Added sample how to customize User ID based on identity

--- Flexmonster Accelerator MVC/Controllers/AcceleratorController.cs
+++ Flexmonster Accelerator MVC/Controllers/AcceleratorController.cs
@@ -1,9 +1,21 @@
 ﻿using Flexmonster.Accelerator.Models;
+using System.Security.Principal;
 
 namespace Flexmonster_Accelerator_MVC.Controllers
 {
     public class AcceleratorController : Flexmonster.Accelerator.Controllers.FlexmonsterProxyController
     {
+        protected override void SetPrincipal(BaseArgs args, IPrincipal principal)
+        {
+            if (principal != null)
+            {
+                // set user name/email here
+                // should be Base64 encoded string in "<usrename>:<password>" fromat
+                args.credentials = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes("xxx"));
+            }
+            base.SetPrincipal(args, principal);
+        }
+
         public override void OnRequest(BaseArgs args)
         {
             base.OnRequest(args);
